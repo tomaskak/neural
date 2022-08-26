2438548fc99c698900900622215a7c3a82a9f6c4
Tomas Kaknevicius <tkaknevicius@gmail.com>
2022-08-26 10:59:41 -0700

    bug

--- neural/util/exp_replay.py
+++ neural/util/exp_replay.py
@@ -232,6 +232,7 @@ class SplitExpReplayReader:
 
         self._pct_one -= self._decrement
 
+        print(f"sample_one={sample_one}, sample_two={sample_two}")
         return [np.concatenate(one, two) for one, two in zip(sample_one, sample_two)]
 
 class ExpReplayCore:
